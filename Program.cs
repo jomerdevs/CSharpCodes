29126e8d30e7a75f56d88e1f60ea7db9dce5746c
jomsanchez <jomsanchez@PORTA-3420-L106>
2023-02-23 14:05:53 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+using CSharpCodes;
+
+int[] numbers = { 38, 27, 43, 3, 9, 82, 10 };
+int length = numbers.Length;
+Console.WriteLine("Antes del Merge Sort: ");
+foreach (int item in numbers)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine();
+
+Console.WriteLine("Después del Merge Sort: ");
+MergeSortAlgorithm.SortMethod(numbers, 0, length-1);
+foreach (int item in numbers)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine();
+
